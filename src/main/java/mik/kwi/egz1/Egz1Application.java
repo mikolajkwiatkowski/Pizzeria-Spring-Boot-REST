51af0d86316168612f27fc13c5d51446b061ea9e
Mikolaj Kwiatkowski <jestemczarny2137@gmail.com>
2024-06-19 19:43:00 +0200

    xd

---
+++ src/main/java/mik/kwi/egz1/Egz1Application.java
@@ -0,0 +1,13 @@
+package mik.kwi.egz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Egz1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Egz1Application.class, args);
+	}
+
+}
---
+++ src/main/java/mik/kwi/egz1/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package mik.kwi.egz1.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
